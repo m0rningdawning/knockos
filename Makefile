fd35c104b77f2f933c7f035b3ff601cfeceb11c3
Pavlo Korolov <bibonga54@gmail.com>
2024-12-10 15:37:40 +0100

    Fake BPB for real hardware

---
+++ Makefile
@@ -0,0 +1,12 @@
+SRC_DIR = ./src
+BIN_DIR = ./bin
+BOOT_SRC = $(SRC_DIR)/boot.asm
+BOOT_BIN = $(BIN_DIR)/boot.bin
+
+$(BOOT_BIN): $(BOOT_SRC)
+	nasm -f bin $(BOOT_SRC) -o $(BOOT_BIN)
+
+clean:
+	rm -f $(BOOT_BIN)
+
+.PHONY: clean
--- src/boot.asm
+++ src/boot.asm
@@ -1,14 +1,27 @@
 ORG 0 ; start from the beginning of the address space
 BITS 16 ; real mode 16 bits
 
-jmp 0x7c0: start
+; start from jmp short to actual start routine to free up
+; space to fake the bpb for hardware bios differences
+_start:
+    jmp short start
+    nop
+
+times 33 db 0 ; fake bios parameter block 33 bits
 
 start:
-    cli ; clear ints then set registers so that the boot loader origin is set correctly
+    jmp 0x7c0: continue_start ; continue the start procedure
+
+continue_start:
+    ; clear ints then set registers so that the boot
+    ; loader origin is set correctly
+    cli
     mov ax, 0x7c0
     mov ds, ax
     mov es, ax
-    mov ax, 0x00 ; clear stack segment then set the stack pointer to bootloader space
+    ; clear stack segment then set the stack 
+    ; pointer to bootloader space
+    mov ax, 0x00 
     mov ss, ax
     mov sp, 0x7c00
     sti ; set ints
@@ -38,3 +51,4 @@ message: db 'Hello World!', 0
 
 times 510 - ($ - $$) db 0 ; fill the rest of the bytes with 0
 dw 0xAA55 ; signature
+
