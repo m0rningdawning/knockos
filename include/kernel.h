76afbde94833f62323712b9a29464c0f1ea64ac1
Pavlo Korolov <bibonga54@gmail.com>
2025-06-22 17:55:38 +0200

    Utils + OOB check

--- include/kernel.h
+++ include/kernel.h
@@ -7,10 +7,11 @@
 #define VGA_WIDTH 80
 #define VGA_HEIGHT 20
 
+void kernel_main();
+size_t strlen(const char *str);
 uint16_t make_char(const char c, const uint8_t color);
 void clear_screen(const uint8_t color);
 void write_chars(const char *string, const uint8_t color, const uint8_t pos_x,
                  const uint8_t pos_y);
-void kernel_main();
 
 #endif /* ifndef KERNEL_H */
--- src/kernel.c
+++ src/kernel.c
@@ -1,5 +1,11 @@
 #include "../include/kernel.h"
 
+size_t strlen(const char *str) {
+    size_t len = 0;
+    while (str[len]) len++;
+    return len;
+}
+
 uint16_t make_char(const char c, const uint8_t color) {
   return (color << 8) | (uint8_t)c;
 }
@@ -7,13 +13,17 @@ uint16_t make_char(const char c, const uint8_t color) {
 void clear_screen(const uint8_t color) {
   volatile uint16_t *video = (volatile uint16_t *)0xB8000;
 
-  for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH; i++) {
+  for (int i = 0; i < VGA_HEIGHT * VGA_WIDTH; ++i) {
     *video++ = make_char(' ', color);
   }
 }
 
 void write_chars(const char *string, const uint8_t color, const uint8_t pos_x,
                  const uint8_t pos_y) {
+  if ((pos_x + strlen(string) > VGA_WIDTH) || (pos_y > VGA_HEIGHT)) {
+    write_chars("Error: Out of bounds", 4 /*Red bg*/, 0, 0);
+    return;
+  }
   volatile uint16_t *video =
       (volatile uint16_t *)0xB8000 + VGA_WIDTH * pos_y + pos_x;
 
@@ -27,12 +37,12 @@ void write_chars(const char *string, const uint8_t color, const uint8_t pos_x,
 }
 
 void kernel_main() {
-  uint8_t pos_x = 0;     // (default == 80)
-  uint8_t pos_y = 0;     // (default == 20)
+  uint8_t pos_x = VGA_WIDTH / 2 - 6;     // (pos_x <= 80 - strlen)
+  uint8_t pos_y = VGA_HEIGHT / 2;     // (pos_y <= 20)
   uint8_t text_clr = 2;  // green color
   uint8_t crear_clr = 0; // default black clear color
 
   clear_screen(crear_clr);
-  write_chars("Hello\nWorld!", text_clr, pos_x, pos_y);
+  write_chars("Hello World!", text_clr, pos_x, pos_y);
 }
 
