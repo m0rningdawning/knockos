ecbf1c34633b39214dd6974f069ebeb023599dbc
Pavlo Korolov <bibonga54@gmail.com>
2025-06-22 15:06:11 +0200

    C kernel hello world

--- src/kernel.c
+++ src/kernel.c
@@ -1,3 +1,13 @@
 #include "../include/kernel.h"
 
-void kernel_start() {}
+void write_string(const char *string, int color) {
+  volatile char *video = (volatile char *)0xB8000;
+
+  while (*string != 0) {
+    *video++ = *string++;
+    *video++ = color;
+  }
+}
+
+void kernel_main() { write_string("Hello World!", 2); }
+
