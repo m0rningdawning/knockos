379379052d5db057f3d9433fd1ae315508abe63f
Pavlo Korolov <bibonga54@gmail.com>
2025-06-23 19:39:50 +0200

    IDTR definition

--- include/idt.h
+++ include/idt.h
@@ -4,12 +4,17 @@
 #include <stdint.h>
 
 // OSDev.org IDT
-struct idt_sesc {
+struct idt_desc {
   uint16_t offset_1;
   uint16_t selector;
   uint8_t  zero;
   uint8_t  type_attr;
   uint16_t offset_2;
+} __attribute__((packed));
+
+struct idtr_desc {
+  uint16_t limit;
+  uint32_t base;
 } __attribute__((packed));
 
 #endif /* ifndef IDT_H */
