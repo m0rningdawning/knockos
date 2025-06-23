ba2d928ae4745ab1ff02679d1a0c09745e62b751
Pavlo Korolov <bibonga54@gmail.com>
2025-06-23 19:32:19 +0200

    IDT definition

---
+++ include/idt.h
@@ -0,0 +1,15 @@
+#ifndef IDT_H
+#define IDT_H
+
+#include <stdint.h>
+
+// OSDev.org IDT
+struct idt_sesc {
+  uint16_t offset_1;
+  uint16_t selector;
+  uint8_t  zero;
+  uint8_t  type_attr;
+  uint16_t offset_2;
+} __attribute__((packed));
+
+#endif /* ifndef IDT_H */
--- src/kernel.c
+++ src/kernel.c
@@ -1,9 +1,11 @@
 #include "../include/kernel.h"
 
 size_t strlen(const char *str) {
-    size_t len = 0;
-    while (str[len]) len++;
-    return len;
+  size_t len = 0;
+  while (str[len]) {
+    len++;
+  }
+  return len;
 }
 
 uint16_t make_char(const char c, const uint8_t color) {
@@ -21,7 +23,7 @@ void clear_screen(const uint8_t color) {
 void write_chars(const char *string, const uint8_t color, const uint8_t pos_x,
                  const uint8_t pos_y) {
   if ((pos_x + strlen(string) > VGA_WIDTH) || (pos_y > VGA_HEIGHT)) {
-    write_chars("Error: Out of bounds", 4 /*Red bg*/, 0, 0);
+    write_chars("Error: Out of bounds", 4 /*red bg*/, 0, 0);
     return;
   }
   volatile uint16_t *video =
@@ -37,10 +39,10 @@ void write_chars(const char *string, const uint8_t color, const uint8_t pos_x,
 }
 
 void kernel_main() {
-  uint8_t pos_x = VGA_WIDTH / 2 - 6;     // (pos_x <= 80 - strlen)
-  uint8_t pos_y = VGA_HEIGHT / 2;     // (pos_y <= 20)
-  uint8_t text_clr = 2;  // green color
-  uint8_t crear_clr = 0; // default black clear color
+  uint8_t pos_x = VGA_WIDTH / 2 - 6; // (pos_x <= 80 - strlen)
+  uint8_t pos_y = VGA_HEIGHT / 2;    // (pos_y <= 20)
+  uint8_t text_clr = 2;              // green color
+  uint8_t crear_clr = 0;             // default black clear color
 
   clear_screen(crear_clr);
   write_chars("Hello World!", text_clr, pos_x, pos_y);
